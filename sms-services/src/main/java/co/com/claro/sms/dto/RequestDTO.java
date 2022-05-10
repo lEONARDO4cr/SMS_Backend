115de61ab13d0692e0ce3d51aa17f264b601e661
lEONARDO <104219998+lEONARDO4cr@users.noreply.github.com>
2022-05-10 13:19:15 -0500

    agregar parametro biHeaderId

--- sms-services/src/main/java/co/com/claro/sms/dto/RequestDTO.java
+++ sms-services/src/main/java/co/com/claro/sms/dto/RequestDTO.java
@@ -12,17 +12,19 @@ public class RequestDTO implements Serializable {
 
 	private String phone;
 	private String message;
-	
+
 	private TypeDocument clientTypeDocument;
 	private String clientDocument;
 	private String clientAccount;
 	private String clientEmail;
 	private String clientName;
-	
+
 	private TypeDocument asesorTypeDocument;
 	private String asesorDocument;
 	private String asesorCod;
 	private String asesorName;
 	private String asesorSignum;
+
+	private String biHeaderId;
 
 }
