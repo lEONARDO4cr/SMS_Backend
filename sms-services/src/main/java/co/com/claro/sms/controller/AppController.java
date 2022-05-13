1d51e6b3d8909dcec07214740dcda7cf9af89e4f
lEONARDO <104219998+lEONARDO4cr@users.noreply.github.com>
2022-05-13 09:40:50 -0500

    CrossOrigin

--- sms-services/src/main/java/co/com/claro/sms/controller/AppController.java
+++ sms-services/src/main/java/co/com/claro/sms/controller/AppController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin
 public class AppController {
 
 	@Autowired
